bbf3e53b62acfed547e3275a27f02c64b907ec6a
iradtaufique <iradukunda.ta@gmail.com>
2022-07-26 12:43:18 +0200

    configure image upload with multer

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer')
+const User = require('../models/users')
+
+
+// Image Upload
+var storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './uploads');
+  },
+  filename: function(req, file, cb){
+    cb(null, file.filename + '_' + Date.now() + '_' + file.originalname)
+  },
+})
+
+var Upload = multer({
+  storage: storage
+}).single('image');
 
 
 // HomePage Router
